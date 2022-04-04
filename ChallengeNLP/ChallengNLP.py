d2b0b2f014f2c5b0cfff5cd15035a1d8209eebbb
Yashashwee Chakrabarty <40659062+Yashashwee@users.noreply.github.com>
2022-04-04 15:44:42 +0530

    Added separate files for testing

--- ChallengeNLP/ChallengNLP.py
+++ ChallengeNLP/ChallengNLP.py
@@ -3,18 +3,20 @@ import sys
 
 n=len(sys.argv)
 if n != 2:
+    print("Invalid number of args")
     exit()
-
-currency_reg='[$₹£][^\s][0-9]*[\.]{0,1}[0-9]*'
-date_reg = '[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]|[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]'
-card_reg =  '\s[4-9][4-9]*th|first|second|\s[a-zA-Z]+enth'
-vowel_reg = '\s[aeiou][a-zA-Z]{3}\s|\s[AEIOU][a-zA-Z]{3}\s'
-
 url = sys.argv[1]
+
+#defining our regexes
+curr='[$₹£][^\s][0-9]*[\.]{0,1}[0-9]*'
+date = '[0-2][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]|3[0-1]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]|[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]'
+card =  '\s[4-9]th|\s[0-9][4-9]th|first|second|third|fourth|fifth|sixth|seventh|eighth|nine?th|tenth|eleventh|twelfth|thirteenth|fourteenth|fifteenth|sixteenth|seventeenth|eighteenth|nineteenth|twentieth|thirtieth|fortieth|fiftieth|sixtieth|seventieth|eightieth|ninetieth|\s[a-zA-Z]+enth'
+vowel = '\s[aeiou][a-zA-Z]{3}\s|\s[AEIOU][a-zA-Z]{3}\s'
+
 
 with open(url,encoding='utf-8') as f:
     fileStr = f.read()
-    print(regex.findall(date_reg,fileStr))
-    print(regex.findall(currency_reg,fileStr))
-    print(regex.findall(card_reg,fileStr))
-    print(regex.findall(vowel_reg,fileStr))
+    print(regex.findall(date,fileStr))
+    print(regex.findall(curr,fileStr))
+    print(regex.findall(card,fileStr))
+    print(regex.findall(vowel,fileStr))
